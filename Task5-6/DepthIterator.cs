ee3886432a7daebd52bae4f523a65a658bc6aec4
danon <ipz232_vdb@student.ztu.edu.ua>
2025-05-12 19:46:23 +0300

    2 PR

---
+++ Task5-6/DepthIterator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_6
+{
+    public class DepthIterator : IIterator
+    {
+        public IEnumerable<LightNode> Scan(LightNode root)
+        {
+            var stack = new Stack<LightNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                yield return current;
+
+                if (current is LightElementNode element)
+                {
+                    for (int i = element.Children.Count - 1; i >= 0; i--)
+                        stack.Push(element.Children[i]);
+                }
+            }
+        }
+    }
+}
